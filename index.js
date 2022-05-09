f401aae4324afe793d561f8c641deec801c02909
Windzyboy <63932310+Windzyboy@users.noreply.github.com>
2022-05-09 14:12:04 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const monday = 1;
+const tuesday = 2;
+const wednesday = 3;
+const thursday = 4;
+const friday = 5;
+const saturday = 6;
+const sunday = 7;
+
+function updateSchedule(day, ) {
+    
+}
+
+fetch().then()
